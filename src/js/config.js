9066cad5edb83f8fdfad344d03fb1fabbbcd5470
Ricardo Garcia <ricardonunesgarcia@outlook.com>
2023-02-07 14:09:32 -0300

    Subindo arquivos do projeto

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SECONDS = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a08c5bc7-9f44-4a8f-8f44-ead3fcb1ab37';
+export const MODAL_CLOSE_SEC = 2.5;
